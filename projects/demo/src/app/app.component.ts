9cb2302685e8364a5fb00daee09967c94136106a
t.a.d.d.e <nicola.taddei.94@gmail.com>
2019-11-19 00:00:01 +0100

    refactor(demo): restyle demo

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { NgxProgressService } from '../../../lib/src/lib/ngx-progress.service';
+import { ColorEvent } from 'ngx-color';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +9,34 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
-  barColor = '#0984e3';
-  showSpinner = false;
+  color = '#0984e3';
+  showSpinner = true;
   showBar = true;
-  barHeight = '1px';
-  spinnerDiameter = '10px';
+  barHeight = '2px';
+  spinnerDiameter = '15px';
   initialValue: number = undefined;
   overlay = true;
   spinnerSpeed: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 = 4;
+  showPicker = false;
+
+  @HostListener('click', ['$event']) onClick(event: MouseEvent): void {
+    if (
+      !(event.target as HTMLElement).className.includes('block') &&
+      !(event.target as HTMLElement).className.includes('square') &&
+      !((event.target as HTMLElement).nodeName === 'INPUT')
+    ) {
+      this.showPicker = false;
+    }
+  }
+
+  constructor(private readonly progress: NgxProgressService) {
+    this.progress.start();
+  }
+
+  changeColor(event: ColorEvent): void {
+    this.color = event.color.hex;
+    if (!(event.$event instanceof KeyboardEvent)) {
+      this.showPicker = false;
+    }
+  }
 }
--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -2,22 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatCheckboxModule, MatFormFieldModule, MatInputModule, MatSliderModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { HttpClientModule } from '@angular/common/http';
-import { NgxProgressModule } from '../../../lib/src/lib/ngx-progress.module';
+import { NgxProgressOnlyBarModule } from '../../../lib/src/lib/ngx-progress.module';
+import { FormsModule } from '@angular/forms';
+import { ColorBlockModule } from 'ngx-color/block';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule,
     MatFormFieldModule,
     MatInputModule,
     FlexLayoutModule,
-    MatButtonModule,
-    NgxProgressModule,
+    NgxProgressOnlyBarModule,
+    MatCheckboxModule,
+    FormsModule,
+    ColorBlockModule,
+    MatSliderModule,
   ],
   bootstrap: [AppComponent],
 })
